09ac1504a68b7e9c76038914558c8431bc36c7db
Rafael <78911513+R-Gasanov@users.noreply.github.com>
2024-11-17 15:33:15 +0000

    1.9 Out Of Bounds
    Managed to make an out of bounds border around the background when your doing the arcade level segment

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Used to change into different scenes
 
 public class OutOfBounds : MonoBehaviour
 { // Okay, so based on what I've learned creating the SceneLoadProjector C#
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    // Here if the player goes out of the background scene it will atomatically be ejected out of the scene
+
+    private bool PlayerInBound = true;
+
     void Update()
     {
-        
+        if (PlayerInBound == false)
+        { // --> In a future update I'll add a screen to show the player lost the level
+
+            // If the player is indeed out of the bound of 
+            EjectScene();
+        }
+    }
+
+    // --> The function used to eject the player back into the main scene
+    void EjectScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // --> The checker to see if the user has indeed left the scene
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInBound = false;
+        }
     }
 }
