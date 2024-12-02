944a2732e2f235c43bd333d21e1c9c1e5633b6c7
Rafael <78911513+R-Gasanov@users.noreply.github.com>
2024-12-02 15:56:03 +0000

    2.4 All Levels Complete
    Managed to complete finish all the levels within the game

--- Assets/Scripts/MainScene/ProjectorUpdate.cs
+++ Assets/Scripts/MainScene/ProjectorUpdate.cs
@@ -10,6 +10,12 @@ public class ProjectorUpdate : MonoBehaviour
     public GameObject On_Projector_01; // --> The current object that is inactive
     public GameObject Idle_Projector_02;
     public GameObject On_Projector_02;
+    public GameObject Idle_Projector_03;
+    public GameObject On_Projector_03;
+    public GameObject Idle_Projector_04;
+    public GameObject On_Projector_04;
+    public GameObject Idle_Projector_05;
+    public GameObject On_Projector_05;
 
     // Start is called before the first frame update
     private void Start()
@@ -27,11 +33,34 @@ public class ProjectorUpdate : MonoBehaviour
             On_Projector_02.SetActive(true);
 
             // We are slowly scaling her difficulty to make the game more harder
-
-
             HerSpawn.SpawnChance = 0.2f;
             HerMovement.speed = 8f;
 
         }
+        else if (LevelCompleteLoader.CompleteLevel && Idle_Projector_03 != null && On_Projector_03 != null && SceneLoaderProjector.CurrentLevel.LvlCount == 4)
+        { // --> The level will become excessively more difficult as time goes on
+            Idle_Projector_03.SetActive(false);
+            On_Projector_03.SetActive(true);
+
+            HerSpawn.SpawnChance = 0.25f;
+            HerMovement.speed = 12f;
+        }
+        else if (LevelCompleteLoader.CompleteLevel && Idle_Projector_04 != null && On_Projector_04 != null && SceneLoaderProjector.CurrentLevel.LvlCount == 5)
+        {
+            Idle_Projector_04.SetActive(false);
+            On_Projector_04.SetActive(true);
+
+            HerSpawn.SpawnChance = 0.3f;
+            HerMovement.speed = 16f;
+        }
+        else if (LevelCompleteLoader.CompleteLevel && Idle_Projector_05 != null && On_Projector_05 != null && SceneLoaderProjector.CurrentLevel.LvlCount == 6)
+        {
+            Idle_Projector_05.SetActive(false);
+            On_Projector_05.SetActive(true);
+
+            HerSpawn.SpawnChance = 0.35f;
+            HerMovement.speed = 20f;
+        }
+        //else if (LevelCompleteLoader.CompleteLevel && Idle_)
     }
 }
